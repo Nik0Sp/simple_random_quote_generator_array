62ba771bcabfc94beafc32ee04ef16de7830ccef
Nik0Sp <nikopringer@gmail.com>
2023-07-07 10:41:26 +0200

    readme eingefügt

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import QuoteList from "./components/QuoteList";
 function App() {
   return (
     <div className="section">
-      <h1 className="title">Quote App</h1>
+      <h1 className="title">Simple Quote Random Generator</h1>
       <div className="container glass">
         <QuoteList />
       </div>
